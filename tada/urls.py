9dc762fa89fbb4853bb368b31d4cb6bd4e86db03
tada20001 <tada20001@gmail.com>
2020-11-29 17:37:42 +0900

    first commit

---
+++ tada/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+import debug_toolbar
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
